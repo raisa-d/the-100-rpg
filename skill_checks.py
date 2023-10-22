fdb555f552fd6cc715a95e8ad88a16f10fba7353
RaiBread <144272001+raisa-d@users.noreply.github.com>
2023-10-22 13:51:24 -0400

    Update skill_checks.py
    1. added roll_witih_disadvantage() to handle exhaustion penalties
    2. changed intelligence_check() to handle exhaustion lvl 1 penalty
    3. changed const_saving_throw() to handle exhaustion lvl 2 penalty

--- skill_checks.py
+++ skill_checks.py
@@ -1,18 +1,36 @@
 import time as t
 from util import diceRoll, enter, bold, end, green, red
 
+def roll_with_disadvantage(): # handles disadvantaged rolls incurred from exhaustion
+    print("\nDue to exhaustion, you have a disadvantage and will use the lower of two dice rolls.")
+    print("\nRolling first d20...")
+    t.sleep(0.5)
+    roll1 = diceRoll(20)
+    print(f"\nYou rolled a {roll1}")
+    print("\nRolling second d20...")
+    t.sleep(0.5)
+    roll2 = diceRoll(20)
+    print(f"\nYou rolled a {roll2}")
+    t.sleep(0.5)
+    final_roll = min(roll1, roll2) # use the lower of the two rolls
+    print(f"{bold}\nYou got {final_roll}.{end}\n")
+    return final_roll
+
 def roll_d20():
     roll = diceRoll(20)
     print("\nRolling d20...")
-    t.sleep(1)
-    print(f"\nYou rolled {roll}.")
-    t.sleep(1)
+    t.sleep(0.5)
+    print(f"\nYou rolled {roll}.\n")
+    t.sleep(0.5)
     return roll
 
 # intelligence skill check --> investigation, history, arcana, nature, religion
 def intelligence_check(user, difficulty_class): # pass in player and difficulty class of what you're trying to do
-    roll = roll_d20()
-    print("\nAdding your intelligence modifier...")
+    if user.exhaustion_level == 1: # if exhaustion level 1, roll with disadvantage
+        roll = roll_with_disadvantage()
+    else: # if not exhaustion lvl 1, no disadvantage to ability check rolls
+        roll = roll_d20()
+    print("Adding your intelligence modifier...")
     t.sleep(1)
     int_check = roll + user.int_mod
     print(f"\n{bold}You got {int_check}.{end}")
@@ -44,13 +62,16 @@ def dex_check(user, DC):
 
 # constitution saving throw
 def const_saving_throw(user, DC):
-    roll = roll_d20()
+    if user.exhaustion_level == 2: # if exhaustion level 2, disadvantage on saving throws
+        roll = roll_with_disadvantage()
+    else:
+        roll = roll_d20()
     t.sleep(1)
     if user.crime_num == 3: # second child has constitution proficiency and therefore adds proficiency bonus and constitution modifier
-        print("\nAdding your constitution modifier and proficiency bonus...")
+        print("Adding your constitution modifier and proficiency bonus...")
         const_check = roll + user.const_mod + user.prof_bonus
     else:
-        print("\nAdding your constitution modifier...")
+        print("Adding your constitution modifier...")
         const_check = roll + user.const_mod
     print(f"\n{bold}You got {const_check}.{end}")
 
