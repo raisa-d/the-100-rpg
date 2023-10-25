8d0c692e365d6afaa3643df2402d7be05ac11ae1
RaiBread <144272001+raisa-d@users.noreply.github.com>
2023-10-25 13:55:19 -0400

    Create battle.py
    1. Created Battle class
    2. Added a reaction turn to the battle in which you can dodge, heal wounds, or steal gold

---
+++ battle.py
@@ -0,0 +1,181 @@
+import time as t, random as r
+from util import clear, enter, draw, diceRoll, bold, copper, gold, green, end, red, purple, yellow, lime, orange, turquoise, white, blue
+from characters import print_inventory, Player
+from skill_checks import roll_with_disadvantage, roll_d20
+
+class Battle:
+    def __init__(self, plyr, enemy):
+        self.plyr = plyr # plyr refers to player, didn't want to confuse variables by naming this "player"
+        self.enemy = enemy
+    
+    def start_battle(self): # handles battle itself
+        round = 1 # tracking round number
+        while self.enemy.HP > 0 and self.plyr.HP > 0: # while player & enemy are both alive
+            global print_battle_title
+            def print_battle_title(user, enemy): # function to bring battle title and both parties' HP
+                clear()
+                bt = f"{bold}{red}xX  Time to Battle  Xx{end}" # title
+                pr = f"{bold}Round {round}{end}"
+                print(f"{bt:^90}") # print battle screen
+                print(f"{pr:^85}") # print current round number
+                print(f"\n{bold}{purple}{user.name.title()}{end}: {user.HP}/{user.maxHP} HP 🩸\n{bold}{yellow}{enemy.name.title()}{end}: {enemy.HP} /{enemy.maxHP} HP 🩸")
+            
+            while True:
+                print_battle_title(self.plyr, self.enemy) # printing screen title
+                print("\nAttack | Use Item") # battle options
+                attack_or_inv = input("\n> ").strip().lower() # input first move
+
+                if attack_or_inv != "": # if not an empty string
+                    if attack_or_inv[0] == "a": # choose attack
+                        self.plyr_attack()
+                    
+                    elif attack_or_inv[0] in ["u", "i"]: # choose use item
+                        print_inventory(self.plyr) # passing player into inventory function
+                        print_battle_title(self.plyr, self.enemy)
+                        print()
+                        draw()
+                        print("You used an inventory item as your turn.")
+                        draw()
+                    
+                    else: # any other answer
+                        print(f"{red}{bold}Invalid command.\n{green}Valid commands:{end}\n['a', 'attack'\n'use item', 'item', 'inv', 'i', 'u']")
+                        enter()
+                        continue
+                else: # if answer with an empty string
+                    print(f"{red}{bold}Invalid command.\n{green}Valid commands:{end}\n['a', 'attack'\n'use item', 'item', 'inv', 'i', 'u']")
+                    enter()
+                    continue
+
+                if self.enemy.HP <= 0: # if enemy is dead
+                    return True # player wins
+                
+                else: # if enemy is not dead, they will attack
+                    self.reaction_turn()
+                    self.enemy_attack()
+
+                if self.plyr.HP <= 0: # if player dies
+                    print("\nYu gonplei ste odon.\nMay we meet again.")
+                    quit()
+
+                round += 1 # adding onto round
+                enter()
+
+    def plyr_attack(self): # player attack 
+        while True:
+            print_battle_title(self.plyr, self.enemy)
+            if self.plyr.equipped_weapon is not None: # if have a weapon
+                if self.plyr.equipped_weapon.finesse == True: # if finesse weapon, choice of str or dex_mod
+                    str_or_dex = input(f"\n{bold}| {turquoise}Strength ({self.plyr.str_mod}){white} | {blue}Dexterity ({self.plyr.dex_mod}){white} |{end}\n\n> ").strip().lower()
+                
+                if str_or_dex != "":
+                    # if using strength modifier (melee or chose str)
+                    if str_or_dex[0] == "s" or self.plyr.equipped_weapon.melee == True: 
+                        if self.plyr.exhaustion_level == 2: # if exhaustion lvl 2, disadvantage on attack roll
+                            draw()
+                            attack_roll = roll_with_disadvantage() + self.plyr.str_mod
+                            print(f"{bold}You got {attack_roll}.{end}\n")
+                        else:
+                            draw()
+                            attack_roll = roll_d20() + self.plyr.str_mod
+                            print(f"{bold}You got {attack_roll}.{end}\n")
+
+                    # if using dex modifier (range or chose str)
+                    elif str_or_dex[0] == "d" or self.plyr.equipped_weapon.range == True:
+                        if self.plyr.exhaustion_level == 2:
+                            draw()
+                            attack_roll = roll_with_disadvantage() + self.plyr.dex_mod
+                            print(f"{bold}\nYou got {attack_roll}.{end}\n")
+                        else:
+                            draw()
+                            attack_roll = roll_d20() + self.plyr.dex_mod
+                            print(f"{bold}\nYou got {attack_roll}.{end}\n")
+                    else:
+                        print(f"{bold}{red}Invalid command.\n{green}Valid commands:\n{white}'strength', 'dexterity', or the first letter of each word{end}")
+                        enter()
+                        continue
+                else:
+                    print(f"{bold}{red}Invalid command.\n{green}Valid commands:\n{white}'strength', 'dexterity', or the first letter of each word{end}")
+                    enter()
+                    continue
+                
+                # attack and damage rolls
+                if attack_roll >= self.enemy.AC: # if attack roll successful, on to do damage
+                    damage_roll = diceRoll(self.plyr.equipped_weapon.num_of_sides) + self.plyr.str_mod
+                    self.enemy.HP -= damage_roll
+                    print(f'{bold}{lime}You are adept with your {self.plyr.equipped_weapon.name} and deal the {self.enemy.name} {damage_roll} damage!{end}')
+                    draw()
+                    break
+                else: # if attack roll unsuccessful
+                    print(f"{bold}{orange}You miss them and deal no damage.{end}")
+                    draw()
+                    break
+
+            else: 
+                pass ### code for unarmed strikes
+
+    def reaction_turn(self): # player reaction turn
+        enter()
+        while True:
+            print_battle_title(self.plyr, self.enemy) # printing screen title # clear screen and print updated title screen
+
+            # reaction turn
+            print(f"\n{bold}{copper}| Reaction Turn |{end}")
+            print(f"\n{bold}{turquoise}| Dodge {green}| Heal Wounds {gold}| Steal Gold |{end}")
+            reaction = input("\n> ").strip().lower()
+            
+            if reaction != "": # if reaction is not an empty string
+                if reaction[0] == "d": # if choose dodge
+                    self.plyr.toggle_dodge()
+                    draw()
+                    print("You try to dodge the enemy") ### IMPLEMENT DODGING
+                    break
+
+                elif reaction[0] == "h": # if choose heal wounds
+                    possible_HP = ["1", "2", "3"]
+                    HP_gained = r.choice(possible_HP) # randomly choose how much they heal
+                    
+                    self.plyr.HP += int(HP_gained) # give player the HP
+                    draw()
+                    print(f"\n{green}{bold}You gained back {HP_gained} HP 🩸{end}")
+                    break
+                
+                elif reaction[0] in ["s", "g"]: # if choose steal gold
+                    possible_amounts = [1, 2, 3, 4, 5]
+                    amt_stolen = r.choice((possible_amounts)) # randomly choose how much GP you steal between 1 and 5
+                    
+                    self.plyr.gp += int(amt_stolen) # give player the GP
+                    draw()
+                    print(f"\n{bold}{gold}You stole {amt_stolen} GP!{end}")
+                    break
+
+                else: # if invalid input
+                    print(f"{bold}{red}Invalid command.\n{green}Valid commands include:\n{white}'dodge', 'heal', 'steal', 'gold'\nor anything that begins with 'd', 'h', 's', or 'g'{end}")
+                    enter()
+                    continue
+            else: # if reaction is an empty string
+                print(f"{bold}{red}Invalid command.\n{green}Valid commands include:\n{white}'dodge', 'heal', 'steal', 'gold'\nor anything that begins with 'd', 'h', 's', or 'g'{end}")
+                enter()
+                continue
+
+    def enemy_attack(self): # enemy attack
+        enemy_str_or_dex = r.choice(("s", "d")) # randomly choose strength or dexterity for enemy
+        
+        if self.enemy.equipped_weapon.melee == True or enemy_str_or_dex == "s": # strength attack roll
+            if self.plyr.is_dodging:
+                enemy_attack_roll = roll_with_disadvantage() + self.enemy.str_mod # apply disadvantage if player dodges
+            else:
+                enemy_attack_roll = diceRoll(20) + self.enemy.str_mod
+        elif self.enemy.equipped_weapon.range == True or enemy_str_or_dex == "d": # dexterity attack roll
+            if self.plyr.is_dodging:
+                enemy_attack_roll = roll_with_disadvantage() + self.enemy.str_mod
+            else:
+                enemy_attack_roll = diceRoll(20) + self.enemy.dex_mod
+            
+        if enemy_attack_roll >= self.plyr.AC: # if attack roll successful, on to do damage
+            enemy_damage_roll = diceRoll(self.enemy.equipped_weapon.num_of_sides) + self.enemy.str_mod
+            self.plyr.HP -= enemy_damage_roll
+            print(f'\n{bold}{yellow}The {self.enemy.name} dealt you {enemy_damage_roll} damage{end}')
+            draw()
+        else:
+            print(f'\n{bold}{yellow}The {self.enemy.name} missed you and dealt no damage!{end}')
+            draw()
